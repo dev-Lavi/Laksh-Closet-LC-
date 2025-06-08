fb1888d199c5aa1e12101d206bad129f4100f4a6
Lavi sharma <lavipandit123@gmail.com>
2025-06-08 14:49:17 +0530

    fixed the wrong import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/header';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import { CartProvider } from './context/CartContext';
 
-import HomePage from './pages/HomePage';
+import HomePage from './pages/homePage';
 import ProductPage from './pages/productpage'; // ✅ import your product page
 
 const Products = () => <div><h1>Our Products</h1></div>;
